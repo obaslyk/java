cdf8a0aeb1d1873e7d64162bcd94467c646d1634
obaslyk <baslykolga24@gmail.com>
2020-11-01 18:04:40 +0300

    Создан класс Point

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,23 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+
+  public double x;
+  public double y;
+
+  public Point (double x1, double y1) {
+    this.x = x1;
+    this.y = y1;
+  }
+
+  public double distanse(Point p2) {
+    return Math.sqrt((p2.x-this.x)*(p2.x-this.x)+(p2.y-this.y)*(p2.y-this.y));
+  }
+
+  public String printPoint() {
+    return this.x + ";" + this.y;
+  }
+
+}
+
+
