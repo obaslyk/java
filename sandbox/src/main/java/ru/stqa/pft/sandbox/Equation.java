b397e2c99ab33f955f976c7a18cee69ebb340a73
obaslyk <baslykolga24@gmail.com>
2020-11-15 17:13:41 +0300

    Добавлены проверки для решения линейного уравнения

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
@@ -14,15 +14,25 @@ public class Equation {
     this.b = b;
     this.c = c;
 
-    double d = b*b - 4*a*c;
-    if (d > 0) {
-      n = 2;
-    }
-    else {
-      if (d == 0) {
+    double d = b * b - 4 * a * c;
+
+    if (a == 0) {
+      if (b == 0) {
+        if (c == 0) {
+          n = -1;
+        } else {
+          n = 0;
+        }
+      } else {
         n = 1;
       }
-      else {
+
+    } else {
+      if (d > 0) {
+        n = 2;
+      } else if (d == 0) {
+        n = 1;
+      } else {
         n = 0;
       }
     }
--- sandbox/src/test/java/ru/stqa/pft/sandbox/EquationTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/EquationTests.java
@@ -19,7 +19,25 @@ public class EquationTests {
 
   @Test
   public void test2() {
-    Equation e = new Equation(1, 5, 1);
+    Equation e = new Equation(1, 5, 6);
     Assert.assertEquals(e.rootNamber(), 2);
+  }
+
+  @Test
+  public void testLinear() {
+    Equation e = new Equation(0, 1, 1);
+    Assert.assertEquals(e.rootNamber(), 1);
+  }
+
+  @Test
+  public void testConstant() {
+    Equation e = new Equation(0, 0, 1);
+    Assert.assertEquals(e.rootNamber(), 0);
+  }
+
+  @Test
+  public void testZero() {
+    Equation e = new Equation(0, 0, 0);
+    Assert.assertEquals(e.rootNamber(), -1);
   }
 }
