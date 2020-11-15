e6b39789a9e2664569e4c4e97b703eaf797105ca
obaslyk <baslykolga24@gmail.com>
2020-11-15 23:36:10 +0300

    Оптимизация заполнения формы добавления группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -6,38 +6,40 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 
 public class HelperBase {
-  protected WebDriver wd;
+    protected WebDriver wd;
 
-  public HelperBase(WebDriver wd) {
-    this.wd = wd;
-  }
-
-  protected void click(By locator) {
-    wd.findElement(locator).click();
-  }
-
-  protected void type(By locator, String text) {
-    click(locator);
-    wd.findElement(locator).clear();
-    wd.findElement(locator).sendKeys(text);
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
+    public HelperBase(WebDriver wd) {
+        this.wd = wd;
     }
-  }
 
-  private boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
+    protected void click(By locator) {
+        wd.findElement(locator).click();
     }
-  }
+
+    protected void type(By locator, String text) {
+        click(locator);
+        if (text != null) {
+            wd.findElement(locator).clear();
+            wd.findElement(locator).sendKeys(text);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            wd.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
 
 }
