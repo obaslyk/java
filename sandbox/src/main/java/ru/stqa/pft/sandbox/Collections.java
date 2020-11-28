7a4b88cb0dab18dd017ded34119cb1d61aa831f3
obaslyk <baslykolga24@gmail.com>
2020-11-28 22:48:29 +0300

    Класс Collections (промежуточный результат)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -1,15 +1,26 @@
 package ru.stqa.pft.sandbox;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Collections {
 
     public static void  main(String[] args) {
         String[] langs = {"Java", "C#", "Python", "PHP"};
 
-//        for (int i = 0; i < langs.length; i++) {
-//            System.out.println("Я хочу выучить " + langs[i]);
-//        }
-        for (String l : langs) {
+        List<String> languages = Arrays.asList("Java", "C#", "Python", "PHP");
+//        List<String> languages = new ArrayList<String>();
+//        languages.add("Java");
+//        languages.add("C#");
+//        languages.add("Python");
+//        languages.add("PHP");
+
+        for (String l : languages) {
             System.out.println("Я хочу выучить " + l);
         }
+//      for (int i = 0; i < langs.length; i++) {
+//           System.out.println("Я хочу выучить " + langs[i]);
+//        }
     }
 }
