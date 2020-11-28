12edcc78534f22fcadff80a15eb0f71613152a46
obaslyk <baslykolga24@gmail.com>
2020-11-28 21:30:44 +0300

    Добавлена функция isPrimeWhile в класс Primes

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Primes.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Primes.java
@@ -10,4 +10,15 @@ public class Primes {
         }
        return true;
     }
+
+    public static boolean isPrimeWhile(int n) {
+        int i = 2;
+        while (i < n) {
+            if (n % i == 0) {
+                return false;
+            }
+            i ++;
+        }
+        return true;
+    }
 }
