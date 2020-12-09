d74df6fa4432cea91b157cbe730f434418daded5
obaslyk <baslykolga24@gmail.com>
2020-12-09 19:53:00 +0300

    Использование уникальных идентификаторов объектов для тестов групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -6,8 +6,6 @@ public class GroupData {
     private String header;
     private String footer;
 
-    public int getId() { return id; }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -15,13 +13,18 @@ public class GroupData {
 
         GroupData groupData = (GroupData) o;
 
+        if (id != groupData.id) return false;
         return name != null ? name.equals(groupData.name) : groupData.name == null;
     }
 
     @Override
     public int hashCode() {
-        return name != null ? name.hashCode() : 0;
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
     }
+
+    public int getId() { return id; }
 
     public GroupData withId(int id) {
         this.id = id;
