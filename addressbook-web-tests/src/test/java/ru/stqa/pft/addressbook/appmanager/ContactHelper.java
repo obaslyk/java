cb998c4b131b3120ed0c19272d417f9b41afb0df
obaslyk <baslykolga24@gmail.com>
2020-12-10 21:50:04 +0300

    Revert "Задание №10: Реализованы проверки в fluent-стиле"
    This reverts commit 6a0652d5

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -7,7 +7,9 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ContactHelper extends HelperBase {
 
